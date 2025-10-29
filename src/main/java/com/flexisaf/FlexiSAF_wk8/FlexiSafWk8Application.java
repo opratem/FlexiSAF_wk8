50a756e5c875259e0487d88bebedc57a24ea4715
opratem <praiseolufemi1@gmail.com>
2025-10-29 15:46:17 +0100

    First commit

---
+++ src/main/java/com/flexisaf/FlexiSAF_wk8/FlexiSafWk8Application.java
@@ -0,0 +1,13 @@
+package com.flexisaf.FlexiSAF_wk8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexiSafWk8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexiSafWk8Application.class, args);
+	}
+
+}
